1decacbc624f0378cf071bb4fb0ad95f3aa9d53e
Rohit Bhanot <rbhanot4739@gmail.com>
2024-11-11 12:47:01 +0530

    Updated yanky config

--- dot_config/nvim/lua/plugins/yanky.lua
+++ dot_config/nvim/lua/plugins/yanky.lua
@@ -1,6 +1,5 @@
 return {
   "gbprod/yanky.nvim",
-  enabled = not vim.env.SSH_TTY,
   keys = {
     { "[P", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
     { "]P", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
